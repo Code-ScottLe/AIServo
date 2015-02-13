48b591ba88c98ab94e9bcea8d668adf9dba882a0
Code-ScottLe <AMRAAMMissiles@hotmail.com>
2015-02-12 21:44:52 -0800

    2nd Commit Test

---
+++ AIServo.Engine/Exposure_Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIServo.Engine
+{
+    public class Exposure_Value
+    {
+        //Base class that hold the data that an exposure value have to support.
+        //I.E basic that hold true for ISO, Apeture and Shutter Speed.
+    }
+}
