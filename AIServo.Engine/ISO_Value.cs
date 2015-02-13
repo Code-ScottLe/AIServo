4cb39a98a1fd0d1ea64bbc68c468d1f2c37e822f
Code-ScottLe <AMRAAMMissiles@hotmail.com>
2015-02-12 22:43:17 -0800

    Local Sync for ISO and Shutter

---
+++ AIServo.Engine/ISO_Value.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIServo.Engine
+{
+    public class ISO_Value : Exposure_Value
+    {
+        private System.Collections.Generic.List<string> ISO_Range;
+    }
+}
---
+++ AIServo.Engine/ShutterSpeed_Value.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AIServo.Engine
+{
+    public class ShutterSpeed_Value : Exposure_Value
+    {
+        private List<string> ShutterSpeed_Range;
+    }
+}
