d2ae1f57dee840b2a523df7d30d02fa592616299
Code-ScottLe <AMRAAMMissiles@hotmail.com>
2015-02-16 00:05:08 -0800

    ISO Class completed.

--- AIServo.Engine/ISO_Value.cs
+++ AIServo.Engine/ISO_Value.cs
@@ -10,9 +10,25 @@ namespace AIServo.Engine
         //Fields
 
         //ISO Range Table.
-        private System.Collections.Generic.List<string> ISO_Range = new List<string>() { "50", "64", "80","100","125","160","200","250","320","400",
+        //Subject to be changed in later build. 
+        private static System.Collections.Generic.List<string> ISO_Range = new List<string>() { "50", "64", "80","100","125","160","200","250","320","400",
             "500","640","800","1000","1250","1600","2000","2500","3200","4000","5000","6400","8000","10000","12800","16000",
             "20000","25600","32000","40000","51200","64000","80000","102400"};
+
+        //Property(ies)
+        public string ISO
+        {
+            get
+            {
+                return _value;
+            }
+
+            set
+            {
+                //note : no logic check.
+                _value = value;
+            }
+        }
 
 
         //Constructor
@@ -43,30 +59,85 @@ namespace AIServo.Engine
         //Method(s)
         public override Exposure_Value OneThirdStep_Increment()
         {
-            return null;
+           /*Override method
+            * Increment the current value of ISO to have +1/3 EV compare to old one*/
+
+            //Back up the current value for return
+            ISO_Value old_value = new ISO_Value(this.ISO);
+            
+            //Get the current ISO's index on the table
+            int index = ISO_Range.IndexOf(ISO);
+
+            //Check if it is the higheset ISO already
+            if (index != (ISO_Range.Count -1))
+            {
+                //If that is the case, increment.
+                ISO = ISO_Range[index + 1];
+            }
+
+            //return the old value
+            return old_value;
         }
 
         public override Exposure_Value OneThirdStep_Decrement()
         {
-            return null;
+            /*Override method
+           * Increment the current value of ISO to have -1/3 EV compare to old one*/
+
+            //Back up the current value for return
+            ISO_Value old_value = new ISO_Value(this.ISO);
+
+            //Get the current ISO's index on the table
+            int index = ISO_Range.IndexOf(ISO);
+
+            //Check if it is the lowest ISO already
+            if (index != 0)
+            {
+                //If that is the case, decrement.
+                ISO = ISO_Range[index - 1];
+            }
+
+            //return the old value
+            return old_value;
         }
 
 
         //Operator Overload
         public static ISO_Value operator ++(ISO_Value var)
         {
+            //Default to be +1/3EV
+            //Call the instance 1/3 Step Increment
+            var.OneThirdStep_Increment();
 
+            //return itself
+            return var;
         }
 
 
         public static ISO_Value operator --(ISO_Value var)
         {
+            //Default to be -1/3EV
+            //Call the instance 1/3 Step Decrement
+            var.OneThirdStep_Decrement();
 
+            //return itself
+            return var;
         }
 
-        public static ISO_Value operator -(ISO_Value var1, ISO_Value var2)
+        public static int operator -(ISO_Value var1, ISO_Value var2)
         {
+            /*Overload for oprator -
+             * return the different in EV step in term of 1/3 step count between 2 value
+             * */
 
+            //get the index in the table for the first value
+            int index_1 = ISO_Range.IndexOf(var1.ISO);
+
+            //get the index in the table for the 2nd value
+            int index_2 = ISO_Range.IndexOf(var2.ISO);
+
+            //return the different
+            return index_1 - index_2;
         }
     }
 }
